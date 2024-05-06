2df6d340cedfd3f01e583cf838c4d91be721b4a8
tegar-wahyu <tegar.wahyu@ui.ac.id>
2024-05-07 00:32:52 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ Berikut adalah urutan eksekusi kode:
 4. Setelah me-spawn task, "hola!" di-print oleh `main`.
 5. Spawner di-drop, menandakan tidak ada task yang akan di-spawn lagi
 6. `executor.run()` dipanggil, mengeksekusi task yang ada di queue.
+
+## 1.3 Multiple Spawn and removing drop
+![MultiSpawn.png](images/MultiSpawn.png)
+Pada gambar di atas, output yang dihasilkan ketika dilakukan multiple spawn tetap diprint
+setelah "hola!". Akan tetapi, urutan eksekusi akan menjadi random karena setiap task dijalankan
+secara asynchronous dan independen. Kemudian, apabila menghapus perintah `drop` program akan
+berjalan terus karena program tidak mengetahui bahwa tidak ada task yang akan dijalankan lagi.
--- src/main.rs
+++ src/main.rs
@@ -107,10 +107,24 @@ fn main() {
         println!("Tegar's Computer: done!");
     });
 
+    spawner.spawn(async {
+        println!("Tegar's Computer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Tegar's Computer: done2!");
+    });
+
+    spawner.spawn(async {
+        println!("Tegar's Computer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Tegar's Computer: done3!");
+    });
+
     println!("Tegar's Computer: hola!");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
-    drop(spawner);
+    // drop(spawner);
 
     // Run the executor until the task queue is empty.
     // This will print "howdy!", pause, and then print "done!".
